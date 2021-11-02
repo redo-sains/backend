2902b352fc09782d3c0a8fe45b3371f23a74100e
redo-sains <redoxsains@gmail.com>
2021-11-02 11:53:15 +0700

    updated

---
+++ src/routes/wishlist.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { requireSignin, userMiddleware } = require('../common-middleware');
+const { addItemToWishlist, getWishlistItems, removeWishlistItems } = require('../controllers/wishlist');
+
+
+
+const router = express.Router();
+
+
+router.post('/user/wishlist/addtowishlist', requireSignin, userMiddleware, addItemToWishlist);
+router.post('/user/getWishlistItems', requireSignin, userMiddleware, getWishlistItems);
+router.post(
+    '/user/wishlist/removeItem',
+    requireSignin,
+    userMiddleware,
+    removeWishlistItems
+  );
+
+module.exports = router;    
